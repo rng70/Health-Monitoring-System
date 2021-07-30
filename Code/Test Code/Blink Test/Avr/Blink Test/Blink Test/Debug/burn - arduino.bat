081338a49d951be287b6530d87e8a0d8731c9b72
Al Arafat Tanin <1705070@ugrad.cse.buet.ac.bd>
2021-07-30 15:28:21 +0600

    Delete Code/Test Code directory

--- Code/Test Code/Blink Test/Avr/Blink Test/Blink Test/Debug/burn - arduino.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title code_burnATMega16
-color 07
-echo !!Welcome to usbasp programmer!!
-echo Please enter the hex file path
-avrdude -c usbasp -p m16 -u -U flash:w:Blink Test.hex
-pause
